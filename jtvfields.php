e3edd39ddfbf26eaa19c480292594546748c3cd5
jneubauer <jon@jonneubauer.com>
2015-07-31 17:48:16 -0400

    refactoring onContentBeforeSave - I think this works ... maybe

--- jtvfields.php
+++ jtvfields.php
@@ -14,91 +14,54 @@ class plgContentJtvfields extends JPlugin {
          */
  
         protected $autoloadLanguage = true;
- 
         function onContentPrepareForm($form, $data) {
-        
                 $app = JFactory::getApplication();
                 $option = $app->input->get('option');
  
                 switch($option) {
- 
                     case 'com_content':
                         if ($app->isAdmin()) {
-                                
-                                JForm::addFormPath(__DIR__ . '/forms/');
-                                
-                                //Show specific forms based on categories
-                                $form->loadFile('content', false); 
-                                
+							JForm::addFormPath(__DIR__ . '/forms/');
+							//Show specific forms based on categories
+							$form->loadFile('content', false);     
                         }
                         if ($app->isSite()) {
-                                
-                                JForm::addFormPath(__DIR__ . '/forms');
-                                
-                                //Show specific forms based on categories
-                                $form->loadFile('content', false); 
-                                
+							JForm::addFormPath(__DIR__ . '/forms');
+							//Show specific forms based on categories
+							$form->loadFile('content', false);
                         }
                         return true;
                 }
-
                 return true;
-
         }
-    
-        public function onContentBeforeSave($context, $article, $isNew)
-        {
-
+        public function onContentBeforeSave($context, $article, $isNew) {
             $attribs = json_decode($article->attribs);
-
             //Let's check that the video_url field is filled in
             if(!empty($attribs->tv_video_url)){
-
-                jimport('joomla.filesystem.file');
-                jimport( 'joomla.image.image' );
-
-                if (!class_exists('JFolder')){
-                    jimport('joomla.filesystem.folder');
-                }
-
                 //Set the path for the thumbnails
                 $asset_dir = JPath::clean( JPATH_SITE.'/images/thumbs/' );
-                
                 $url_arr = parse_url($attribs->tv_video_url);
                 if ($url_arr['host']== 'vimeo.com')
                 {
-            
-                    $v_id = $url_arr['path'];
-                    $vim_id = substr($v_id, 1);
-                    $vim_id = 'v' . $vim_id;
-                    $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video$v_id.php"));
-                    copy($hash[0]['thumbnail_large'], $asset_dir.$vim_id.'.jpg');
-                    $attribs->tv_video_id = $vim_id;
+                    $hash = unserialize(file_get_contents('http://vimeo.com/api/v2/video'.$url_arr['path'].'.php'));
+                    copy($hash[0]['thumbnail_large'], $asset_dir.'v'.substr($url_arr['path'], 1).'.jpg');
+                    $attribs->tv_video_id = 'v'.substr($url_arr['path'], 1);
                 }
                 if ($url_arr['host'] == 'youtu.be')
                 {
-                    $v_id = $url_arr['path'];
-                    $v_id = substr($v_id, 1);
-                    $yt_id = 'y' . $v_id;
-                    copy("http://i1.ytimg.com/vi/$v_id/mqdefault.jpg", $asset_dir.$yt_id.'.jpg');
-                    $attribs->tv_video_id = 'y' . $v_id;
+                    copy("http://i1.ytimg.com/vi/$v_id/mqdefault.jpg", $asset_dir.'y'.substr($url_arr['path'], 1).'.jpg');
+                    $attribs->tv_video_id = 'y'.$v_id;
                 }
                 if ($url_arr['host']== 'www.youtube.com')
                 {
-                    parse_str( parse_url( $attribs->tv_video_url, PHP_URL_QUERY ), $yt_arr );
-                    $v_id = $yt_arr['v'];
-                    $yt_id = 'y' . $yt_arr['v'];
-                    copy("http://i1.ytimg.com/vi/$v_id/mqdefault.jpg", $asset_dir.$yt_id.'.jpg');
-                    $attribs->tv_video_id = $yt_id;
-                }               
-                
+                    parse_str( parse_url($attribs->tv_video_url, PHP_URL_QUERY ), $yt_arr );
+                    copy("http://i1.ytimg.com/vi/$v_id/mqdefault.jpg", $asset_dir.'y'.$yt_arr['v'].'.jpg');
+                    $attribs->tv_video_id = 'y'.$yt_arr['v'];
+                }
                 //Set the new $attribs object for the article
                 $article->attribs = json_encode($attribs);
-
             }
-
             return true;
         }
- 
 }
 ?>
